2405a757d73d9d7326edbb2d2d92132a5290389b
pham-huyson87 <pham.huyson87@hotmail.com>
2018-08-15 19:53:44 -0400

    Add jQuery and Bootstrap JS library
    Use of static files of type styles and scripts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,9 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   template: `
     <h2>Hello World</h2>
-    <img src="/assets/images/basic-shield.png"/>
+    <div class="container">
+      <img src="/assets/images/basic-shield.png"/>
+    </div>
   `
 })
 export class AppComponent {
