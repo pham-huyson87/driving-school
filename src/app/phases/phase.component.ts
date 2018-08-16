58a1d1af588287b132f0d039191f98780440ff12
pham-huyson87 <pham.huyson87@hotmail.com>
2018-08-16 07:13:29 -0400

    Add handle to Click event on Button
    Add handle to Click event on Button

--- src/app/phases/phase.component.ts
+++ src/app/phases/phase.component.ts
@@ -6,9 +6,14 @@ import { Component, Input } from '@angular/core'
     <div class="well">
         <h2>{{phase.name}}</h2>
         <div>{{phase.description}}</div>
+        <button class="btn btn-primary" (click)="handlePhaseClick()">Click me!</button>
     </div>
     `
 })
 export class Phase {
     @Input() phase:any
+
+    handlePhaseClick() {
+        console.log('clicked!');
+    }
 }
