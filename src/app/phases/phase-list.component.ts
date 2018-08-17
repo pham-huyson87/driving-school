1ed1bed7bb555c27e21e8c736ef33065e35dc971
pham-huyson87 <pham.huyson87@hotmail.com>
2018-08-17 09:02:53 -0400

    Use of *ngFor Directive
    Use of *ngFor Directive to loop throught Array of elements

--- src/app/phases/phase-list.component.ts
+++ src/app/phases/phase-list.component.ts
@@ -7,35 +7,41 @@ import { Component } from '@angular/core'
         <h1>Phases</h1>
         <hr/>
 
-        <phase  [phase]="phase1"
-                (phaseClick)=handlePhaseClicked($event)></phase>
-        
-        <phase  #phase2
-                [phase]="phase2"></phase>
-
-        <button class="btn btn-info" (click)="phase2.doOperation()">
-            Call Phase2 doOperation()
-        </button>
-
-        <h3>{{phase2.someProperty}}</h3>
+        <div *ngFor="let phase of phases">
+            <phase  [phase]="phase"></phase>
+        </div>
 
     </div>
     `
 })
 export class PhaseListComponent {
-    phase1 = {
-        id: 1,
-        number: '1',
-        name: 'Phase 1',
-        description: 'Préalable au permis d\'apprenti'
-    };
 
-    phase2 = {
-        id: 2,
-        number: '2',
-        name: 'Phase 2',
-        description: 'Conduite dirigée'
-    };
+    phases = [
+        {
+            id: 1,
+            number: '1',
+            name: 'Phase 1',
+            description: 'Préalable au permis d\'apprenti'
+        },
+        {
+            id: 2,
+            number: '2',
+            name: 'Phase 2',
+            description: 'Conduite dirigée'
+        },
+        {
+            id: 3,
+            number: '3',
+            name: 'Phase 3',
+            description: 'Conduite semi-dirigée'
+        },
+        {
+            id: 4,
+            number: '4',
+            name: 'Phase 4',
+            description: 'Conduite semi-dirigée à autonome'
+        },
+    ]
 
     handlePhaseClicked(data) {
         console.log('received: ', data);
