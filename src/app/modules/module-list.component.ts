a9b8913e7b9d22875895ef7f6b15fb28739c62fc
pham-huyson87 <pham.huyson87@hotmail.com>
2018-08-25 09:18:33 -0400

    Use of Class Binding
    Use of Class Binding

--- src/app/modules/module-list.component.ts
+++ src/app/modules/module-list.component.ts
@@ -2,7 +2,12 @@ import { Component, Input } from '@angular/core'
 
 @Component({
     selector: 'module-list',
-    templateUrl:'./module-list.component.html'
+    templateUrl:'./module-list.component.html',
+    styles: [`
+        .green { 
+            background-color: red !important; 
+        } 
+    `]
 })
 export class ModuleList {
     @Input() modules:any
