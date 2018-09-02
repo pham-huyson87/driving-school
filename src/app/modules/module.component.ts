4a89594d4d0fbbbe226f968f145b17762e391b10
pham-huyson87 <pham.huyson87@hotmail.com>
2018-09-01 21:21:13 -0400

    Read Route Parameter
    - Read Route Parameters with ActivatedRoute

--- src/app/modules/module.component.ts
+++ src/app/modules/module.component.ts
@@ -1,11 +1,25 @@
-import { Component, Input } from '@angular/core'
+import { Component, Input, OnInit } from '@angular/core'
+import { ActivatedRoute } from '@angular/router'
 
 @Component({
     selector: 'module',
     template: `
     <div>NOT USED for now</div>
+    {{phaseId}} 
+    {{moduleId}}
     `
 })
-export class Module {
+export class Module implements OnInit {
+
+    phaseId:any
+    moduleId:any
+
     @Input() module:any
+
+    constructor(private route: ActivatedRoute) {}
+
+    ngOnInit(): void {
+        this.phaseId = this.route.snapshot.params['phase_id'];
+        this.moduleId = this.route.snapshot.params['module_id'];
+    }
 }
